8cc0859a8cb4f993a5bc191c5c934b6ee4e3dc3d
Niclas <niclas.o.a.svensson@gmail.com>
2021-12-18 21:36:31 +0100

    fixes

---
+++ Amazing_Race/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class board {
+    private:
+        vector<vector<int16_t>> tasks;
+        vector<int16_t> matrix;
+        vector<int16_t> not_visited_tasks;
+        int16_t max_time, number_tasks, max_points;
+    public:
+        board(int16_t n, int16_t T): tasks(n), matrix((n+2)*(n+2)), number_tasks(n), max_time(T), max_points(0){
+            for(int16_t i = 0; i < n; ++i){
+                int16_t p, t, d;
+                cin >> p >> t >> d;
+                tasks[i] = {p, t, d};
+            }
+            tasks.push_back({0,0,1400});
+            tasks.push_back({0,0,max_time});
+            int16_t value;
+            for (int16_t y = 0; y < number_tasks + 2; ++y){
+                for(int16_t x = 0; x < number_tasks + 2; ++x){
+                    cin >> value;
+                    matrix[(number_tasks + 2)*y + x] = value;
+                }
+            }
+        }
+        int16_t get_travel_time(int16_t to, int16_t from){
+            return matrix[(number_tasks + 2)*from + to];
+        }
+        int16_t get_points(int16_t i){
+            return tasks[i][0];
+        }
+        int16_t get_time(int16_t i){
+            return tasks[i][1];
+        }
+        int16_t get_deadline(int16_t i){
+            return tasks[i][2] != -1 ? tasks[i][2] : 1400;
+        }
+        void print_result(){
+            cout << max_points << endl;
+            for(int16_t i = 0; i < number_tasks; ++i){
+                if(find(not_visited_tasks.begin(), not_visited_tasks.end(), i) != not_visited_tasks.end()){
+
+                } else {
+                    cout << i + 1 << " ";
+                }
+            }
+        }
+        void search(vector<int16_t> original, int16_t i, int16_t t, int16_t p){
+            //cout << "Points: " << p << endl;
+            //cout << "Time: " << t << endl;
+            if(t > get_deadline(i) || t > max_time){
+                //cout << "reached deadline" << endl;
+                return;
+            } else if(i == number_tasks + 1){
+                //cout << "new high" << endl;
+                if (p > max_points){
+                    max_points = p;
+                    not_visited_tasks = original;
+                }
+                return;
+            }
+            for(int16_t j = 0; j < original.size(); ++j){
+                vector<int16_t> new_vector = original;
+                new_vector.erase(new_vector.begin() + j);
+                search(new_vector, original[j], t + get_time(i) + get_travel_time(original[j], i), p + get_points(original[j]));
+            }
+        }
+        void print(){
+            for(int16_t i = 0; i < number_tasks; ++i){
+                cout << get_points(i) << " " << get_time(i) << " " << get_deadline(i) << endl;
+            }
+            for (int16_t y = 0; y < number_tasks + 2; ++y){
+                for(int16_t x = 0; x < number_tasks + 2; ++x){
+                    cout << get_travel_time(x, y) << " ";
+                }
+                cout << endl;
+            }
+        }
+};
+
+int main(){
+    int16_t n, T;
+    cin >> n >> T;
+    board b(n, T);
+    vector<int16_t> vec(n);
+    for(int16_t i = 0; i < n; ++i){
+        vec[i] = i;
+    }
+    vec.push_back(n + 1);
+    b.search(vec, n, 0, 0);
+    b.print_result();
+    return 0;
+}
--- README.md
+++ README.md
@@ -25,12 +25,13 @@ Then provide the input.
 
 The solution to each problem in the table can be found in the corresponding folder in the repo. The links take you to the problem formulation on the Kattis website.
 
-| Nr. | Problem | Language | Status |
-|---:|:---|:---|:---|
-| 1 |[0-1 Sequences](https://open.kattis.com/problems/sequences)| C++ | Done |
-| 2 |[10 Kinds of People](https://kth.kattis.com/problems/10kindsofpeople)| C++ | Done |
-| 3 |[2048](https://open.kattis.com/problems/2048)| C++ | Done |
-| 4 |[Anagram Counting](https://open.kattis.com/problems/anagramcounting)| Python | Done |
-| 5 |[3D Printed Statues](https://open.kattis.com/problems/3dprinter)| C++ | Done |
-| 6 |[A+B Problem](https://open.kattis.com/problems/aplusb)| C++ | Too slow solution |
-| 5 |[Abandoned Animal](https://open.kattis.com/problems/abandonedanimal)| C++ | Done |
+| Problem | Language | Status | Difficulty |
+|:---|:---|:---|---:|
+|[A+B Problem](https://open.kattis.com/problems/aplusb)| C++ | Too slow solution | 8.4 |
+|[Amazing Race](https://open.kattis.com/problems/race)| C++ | Too slow solution | 5.5 |
+|[Abandoned Animal](https://open.kattis.com/problems/abandonedanimal)| C++ | Done | 5.4 |
+|[0-1 Sequences](https://open.kattis.com/problems/sequences)| C++ | Done | 5.3 |
+|[10 Kinds of People](https://kth.kattis.com/problems/10kindsofpeople)| C++ | Done | 5.2 |
+|[Anagram Counting](https://open.kattis.com/problems/anagramcounting)| Python | Done | 3.1 |
+|[2048](https://open.kattis.com/problems/2048)| C++ | Done | 2.8 |
+|[3D Printed Statues](https://open.kattis.com/problems/3dprinter)| C++ | Done | 2.2 |
